4a42dc54268ce969b0daf2c2b52d11b418e994f3
Rahul11-debug <Rahul.kumarundergrade@gmail.com>
2026-06-12 18:51:31 +0530

    Update CORS settings to include production origin

--- server/index.js
+++ server/index.js
@@ -9,10 +9,12 @@ import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
 
-const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+  origin: [
+    "http://localhost:5173",
+    "https://interview-iq-ai-tnh0.onrender.com"
+  ],
+  credentials: true
 }))
 
 app.use(express.json())
